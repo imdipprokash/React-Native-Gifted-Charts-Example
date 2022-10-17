1a576d5ca5ce70a7ce881d06990969d0fbf81c64
Dipprokash <dipprokash2015@gmail.com>
2022-10-17 12:24:18 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import ChartsLine from './src/ChartsLine';
+const App = () => {
+  return (
+    <View>
+      {/* <Text style={{color: '#000'}}>App</Text> */}
+      <ChartsLine />
+    </View>
+  );
+};
+
+export default App;
